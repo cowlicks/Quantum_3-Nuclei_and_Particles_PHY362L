0dc0f56f82d4cefc332414c0eacef9f84b619395
Blake Griffith <blake.a.griffith@gmail.com>
2013-04-13 13:43:51 -0500

    Added constants that help with this homework.

---
+++ hw6/constants.py
@@ -0,0 +1,13 @@
+class Particle(object):
+    def __init__(self, mass):
+        self.m = mass
+
+Gfermi = 2e-4   # MeV * fm**3
+mp  = 938.3     # MeV
+me  = 0.510998  # MeV
+mdpp    = 1232. # MeV
+mpip    = 139.6 # MeV
+hbarc   = 197.   # MeV * fm
+mtau    = 1777.  # MeV
+mmu     = 105.6 # MeV
+md      = 1869.  # MeV
